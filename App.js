3e85c190e424d0230a34fe8c4f456f4785b1935a
Amrindergill666 <gillamrinder733@gmail.com>
2025-07-31 09:56:23 +0530

    last push

--- App.js
+++ App.js
@@ -6,30 +6,49 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { useEffect } from 'react';
-import { Alert, Platform, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  Alert,
+  Image,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Switch,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+  Text
+} from 'react-native';
 import { PermissionsAndroid } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import OneMsg from './compoents/onesignal';
 import LottieView from 'lottie-react-native';
 import Native from './compoents/native';
-import { NavigationContainer, DefaultTheme, DarkTheme, } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RefreshButton from './compoents/restart';
+import {
+  CopilotProvider,
+  CopilotStep,
+  useCopilot,
+  walkthroughable,
+} from 'react-native-copilot';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 globalThis.RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
 const Stack = createNativeStackNavigator();
 
 function App() {
-
-
   // For status Bar padding issue
-  const isAndroid13OrHigher = Platform.OS === 'android' && Platform.Version >= 33;
+  const isAndroid13OrHigher =
+    Platform.OS === 'android' && Platform.Version >= 33;
   const topPadding = isAndroid13OrHigher ? StatusBar.currentHeight : 0;
 
-
   //  FCM permmision
-
 
   // useEffect(() => {
   //   PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
@@ -40,9 +59,6 @@ function App() {
   //   return unsubscribe;
   // }, [])
 
-
-
-  
   // for theming
 
   const isDarkMode = useColorScheme();
@@ -55,7 +71,7 @@ function App() {
       card: '#fafafa',
       text: '#222',
       // Add or override more colors
-    }
+    },
   };
   const MyDarkTheme = {
     ...DarkTheme,
@@ -65,18 +81,135 @@ function App() {
       background: '#121212',
       card: '#222',
       text: '#fff',
-    }
+    },
   };
+  function Apps() {
+    const { start, copilotEvents } = useCopilot();
+    const [secondStepActive, setSecondStepActive] = useState(true);
+    console.log('secondStepActive: ', secondStepActive, ' (line: 89)');
+    const [lastEvent, setLastEvent] = useState(null);
+    console.log('lastEvent: ', lastEvent, ' (line: 91)');
+
+    useEffect(() => {
+      copilotEvents.on('stepChange', step => {
+        setLastEvent(`stepChange: ${step.name}`);
+      });
+      copilotEvents.on('start', () => {
+        setLastEvent(`start`);
+      });
+      copilotEvents.on('stop', () => {
+        setLastEvent(`stop`);
+      });
+    }, [copilotEvents]);
+    const WalkthroughableText = walkthroughable(Text);
+    const WalkthroughableImage = walkthroughable(Image);
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <CopilotStep
+          text="Hey! This is the first step of the tour!"
+          order={1}
+          name="openApp"
+        >
+          <WalkthroughableText style={styles.title}>
+            {'Welcome to the demo of\n"React Native Copilot"'}
+          </WalkthroughableText>
+        </CopilotStep>
+        <View style={styles.middleView}>
+          <CopilotStep
+            active={secondStepActive}
+            text="Here goes your profile picture!"
+            order={2}
+            name="secondText"
+          >
+            <WalkthroughableImage
+              source={{
+                uri: 'https://pbs.twimg.com/profile_images/527584017189982208/l3wwN-l-_400x400.jpeg',
+              }}
+              style={styles.profilePhoto}
+            />
+          </CopilotStep>
+          <View style={styles.activeSwitchContainer}>
+            <Text style={{ color: 'black' }}>
+              Profile photo step activated?
+            </Text>
+            <View style={{ flexGrow: 1 }} />
+            <Switch
+              onValueChange={secondStepActive =>
+                setSecondStepActive(secondStepActive)
+              }
+              value={secondStepActive}
+            />
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={() => start()}>
+            <Text style={styles.buttonText}>START THE TUTORIAL!</Text>
+          </TouchableOpacity>
+          <View style={styles.eventContainer}>
+            <Text>{lastEvent && `Last event: ${lastEvent}`}</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <CopilotStep
+            text="Here is an item in the corner of the screen."
+            order={3}
+            name="thirdText"
+          >
+            <WalkthroughableText style={styles.tabItem}>
+              {/* <Ionicons name="apps" size={25} color="#888" /> */}
+              <Text>Apps</Text>
+            </WalkthroughableText>
+          </CopilotStep>
+
+          {/* <Ionicons
+            style={styles.tabItem}
+            name="airplane"
+            size={25}
+            color="#888"
+          />
+          <Ionicons
+            style={styles.tabItem}
+            name="ios-globe"
+            size={25}
+            color="#888"
+          />
+          <Ionicons
+            style={styles.tabItem}
+            name="ios-navigate-outline"
+            size={25}
+            color="#888"
+          /> */}
+
+          {/* <Ionicons
+            style={styles.tabItem}
+            name="ios-rainy"
+            size={25}
+            color="#888"
+          /> */}
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  if (false) {
+    return (
+      <CopilotProvider stopOnOutsideClick androidStatusBarVisible>
+        <Apps />
+      </CopilotProvider>
+    );
+  }
   return (
-    <View style={[styles.container,{paddingTop:topPadding}]}>
-      <StatusBar barStyle={'dark-content'} />
-      <NavigationContainer theme={isDarkMode == 'dark' ? MyDarkTheme : MyLightTheme}>
-        <Stack.Navigator>
-          <Stack.Screen name="Native" component={Native} />
-          <Stack.Screen name="RefreshButton" component={RefreshButton} />
-          {/* Add more screens as needed */}
-        </Stack.Navigator>
-      </NavigationContainer>
+    <View style={[styles.container, { paddingTop: topPadding }]}>
+        <StatusBar barStyle={'dark-content'} />
+        <NavigationContainer
+          theme={isDarkMode == 'dark' ? MyDarkTheme : MyLightTheme}
+        >
+          <Stack.Navigator>
+            <Stack.Screen name="Native" component={Native} />
+            <Stack.Screen name="RefreshButton" component={RefreshButton} />
+            {/* Add more screens as needed */}
+          </Stack.Navigator>
+        </NavigationContainer>
       {/* <NewAppScreen templateFileName="App.tsx" /> */}
     </View>
   );
@@ -85,6 +218,50 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 25,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    backgroundColor:'red'
+  },
+  profilePhoto: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    marginVertical: 20,
+  },
+  middleView: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#2980b9',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 22,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  tabItem: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  activeSwitchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+    alignItems: 'center',
+    paddingHorizontal: 25,
+  },
+  eventContainer: {
+    marginTop: 20,
   },
 });
 
--- compoents/native.js
+++ compoents/native.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Linking, StyleSheet, Text, View } from 'react-native'
+import { Button, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 // import { Camera, useCameraDevice, useCameraDevices } from 'react-native-vision-camera';
 import BiometricsAuth from './BioAuth';
 import BLEComponent from './BlueTooth';
@@ -12,30 +12,24 @@ import MultiLanguageDemo from './translation';
 import { useTheme } from '@react-navigation/native';
 import RefreshButton from './restart';
 import CameraFile from './Camera';
-
+import { useCopilot } from 'react-native-copilot';
 
 const Native = () => {
 
     const { colors } = useTheme();
     console.log('colors: ', colors, ' (line: 18)');
 
- 
-   
+  
+      
+
     return (
-        <View style={{ flex: 1,  backgroundColor: colors.background }}>
-            {/* <Text>Native Component</Text> */}
-            
-            {/* <BiometricsAuth/> */}
-            {/* <BLEComponent /> */}
-            {/* <SensorDemo/> */}
-            {/* <HapticExample/> */}
-            {/* <VibrateExample/> */}
-            {/* <GoogleSignInButton/> */}
-            {/* <PhoneAuthScreen/> */}
-            {/* <MultiLanguageDemo/> */}
-            {/* <RefreshButton /> */}
-            {/* <CameraFile/> */}
-            
+        <View style={{ flex: 1, backgroundColor: colors.background,alignItems:'center' }}>
+            <TouchableOpacity 
+            >
+                <Text style={{color:'white',fontSize:30}}>Submit</Text>
+                
+            </TouchableOpacity>
+        
         </View>
     );
 }
